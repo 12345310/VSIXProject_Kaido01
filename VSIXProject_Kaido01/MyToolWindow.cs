d34a89f0fa33400c333bfff7fe59f78790b9568c
海藤 一臣 <qaswr.cdwef.8642@icloud.com>
2020-09-25 10:28:50 +0900

    プロパティをプロパティウィンドウに公開するの件

---
+++ VSIXProject_Kaido01/MyToolWindow.cs
@@ -0,0 +1,34 @@
+﻿namespace VSIXProject_Kaido01
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("65872cd4-08c5-4c2c-9bba-58bb9a4ec1bb")]
+    public class MyToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyToolWindow"/> class.
+        /// </summary>
+        public MyToolWindow() : base(null)
+        {
+            this.Caption = "MyToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new MyToolWindowControl();
+        }
+    }
+}
---
+++ VSIXProject_Kaido01/MyToolWindowCommand.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+using System.Collections;
+using System.ComponentModel;
+
+namespace VSIXProject_Kaido01
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class MyToolWindowCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("03200f3b-1861-4ac9-b342-b9a08c6e6448");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        private ITrackSelection trackSel;
+        private SelectionContainer selContainer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyToolWindowCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private MyToolWindowCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static MyToolWindowCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in MyToolWindowCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new MyToolWindowCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// Shows the tool window when the menu item is clicked.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Get the instance number 0 of this tool window. This window is single instance so this instance
+            // is actually the only one.
+            // The last flag is set to true so that if the tool window does not exists it will be created.
+            ToolWindowPane window = this.package.FindToolWindow(typeof(MyToolWindow), 0, true);
+            if ((null == window) || (null == window.Frame))
+            {
+                throw new NotSupportedException("Cannot create tool window");
+            }
+
+            IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+
+        private ITrackSelection TrackSelection
+        {
+            get
+            {
+                if (trackSel == null)
+                    trackSel =
+                       GetService(typeof(STrackSelection)) as ITrackSelection;
+                return trackSel;
+            }
+        }
+
+        private ITrackSelection GetService(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSelection()
+        {
+            ITrackSelection track = TrackSelection;
+            if (track != null)
+                track.OnSelectChange((ISelectionContainer)selContainer);
+        }
+
+        public void SelectList(ArrayList list)
+        {
+            selContainer = new SelectionContainer(true, false);
+            selContainer.SelectableObjects = list;
+            selContainer.SelectedObjects = list;
+            UpdateSelection();
+        }
+
+        public void OnToolWindowCreated()
+//        public override void OnToolWindowCreated()
+        {
+            ArrayList listObjects = new ArrayList();
+            listObjects.Add(this);
+            SelectList(listObjects);
+        }
+    }
+}
